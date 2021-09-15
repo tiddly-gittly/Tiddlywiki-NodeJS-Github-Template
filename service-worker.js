7a0851b8bdcbcec39b59b82dce60d699b59179e7
linonetwo <linonetwo@users.noreply.github.com>
2021-09-15 13:42:58 +0000

    deploy: d96ff365d103c9477a8fc1cdc276eb0b2bf2eed6

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"20a363f53511857621cbe57971fe52e3","url":"index.html"},{"revision":"8280c4bb66caa1a72ffcec52e62872da","url":"tiddlywiki5.js"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"TiddlyWikiIconBlack.png"},{"revision":"2c94295d5e6cfa9e5f0b666c4ba1964c","url":"TiddlyWikiIconWhite.png"}]);
+precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"d6389a4d25f2e2c22b91175f09e68b08","url":"index.html"},{"revision":"8280c4bb66caa1a72ffcec52e62872da","url":"tiddlywiki5.js"},{"revision":"713f708b9b2662da54cd38bc98a6483f","url":"TiddlyWikiIconBlack.png"},{"revision":"2c94295d5e6cfa9e5f0b666c4ba1964c","url":"TiddlyWikiIconWhite.png"}]);
 
 registerRoute(
   /\.css$/,
