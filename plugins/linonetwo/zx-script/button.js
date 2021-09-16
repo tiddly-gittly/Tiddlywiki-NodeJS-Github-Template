869515d39d745be76fbf845aeada173e3d66c35b
林东吴 <lindongwu@bytedance.com>
2021-09-16 14:05:00 +0800

    feat: update zx-script

--- plugins/linonetwo/zx-script/button.js
+++ plugins/linonetwo/zx-script/button.js
@@ -18,7 +18,7 @@
       this.computeAttributes();
       const importButton = this.document.createElement('button');
       importButton.classList.add('tc-btn-invisible');
-      importButton.innerHTML = $tw.wiki.getTiddlerText('$:/plugins/linonetwo/zx-script/zx-icon.svg');
+      importButton.innerHTML = `${$tw.wiki.getTiddlerText('$:/plugins/linonetwo/zx-script/zx-icon')}<span class="tc-btn-text tc-button-zx-script-caption">${$tw.wiki.getTiddlerText('$:/plugins/linonetwo/zx-script/zx-button-caption')}</span>`;
       importButton.onclick = this.onExecuteButtonClick.bind(this);
       importButton.title = importButton.ariaLabel = 'ZX';
       parent.insertBefore(importButton, nextSibling);
@@ -29,12 +29,29 @@
      * Event listener of button
      */
     async onExecuteButtonClick() {
-      const title = this.getAttribute('title', 'aaa.mjs');
+      const title = this.getAttribute('title', 'aaa.md');
+      const type = this.getAttribute('type', 'text/vnd.tiddlywiki');
       const stateTiddlerTitle = `$:/state/linonetwo/zx-script/output/${title}`;
       let fileName = title;
       let fileContent = this.getAttribute('content', '');
-      if (!fileName.endsWith('.mjs') && !fileName.endsWith('.md')) {
-        fileName += '.mjs';
+      // add mjs or md to the end
+      if (!fileName.endsWith('.mjs') && !fileName.endsWith('.js') && !fileName.endsWith('.md')) {
+        switch (type) {
+          // try fit everything that may have ```js block into md
+          case 'text/vnd.tiddlywiki':
+          case 'text/plain':
+          case 'text/markdown':
+          case 'text/x-markdown':
+          case 'text/html': {
+            fileName += '.md';
+            break;
+          }
+          case 'application/javascript':
+          default: {
+            fileName += '.mjs';
+            break;
+          }
+        }
       }
 
       $tw.wiki.setText(stateTiddlerTitle, 'text', undefined, '');
