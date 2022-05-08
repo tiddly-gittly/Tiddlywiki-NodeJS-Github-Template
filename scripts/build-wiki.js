1bc846f22e02d20bdbc1df837fee936b25de7aa8
林东吴 <lindongwu@bytedance.com>
2022-05-08 12:07:22 +0800

    fix: vercel .json

--- scripts/build-wiki.js
+++ scripts/build-wiki.js
@@ -16,7 +16,11 @@ module.exports = function build() {
   execAndLog(`rm -rf ${folderToServe}`);
   // npm run build:public
   execAndLog(`cp -r ${repoFolder}/public/ ${folderToServe}`, { cwd: repoFolder });
-  execAndLog(`cp ${repoFolder}/vercel.json ${folderToServe}/vercel.json`, { cwd: repoFolder });
+  try {
+    execAndLog(`cp ${repoFolder}/vercel.json ${folderToServe}/vercel.json`, { cwd: repoFolder });
+  }  catch (error) {
+    console.log(error);
+  }
   // try copy some static assets, don't cause error if some of them been removed by the user
   try {
     // npm run build:public
