d22949c16c8b073e62fd41f195d44e091064f0fa
林东吴 <lindongwu@bytedance.com>
2022-05-08 12:05:26 +0800

    chore: readonlyexternalimages

--- scripts/build-wiki.js
+++ scripts/build-wiki.js
@@ -13,27 +13,29 @@ const execAndLog = (command, options) => console.log(String(execSync(command, op
 
 module.exports = function build() {
   // npm run build:prepare
-  execAndLog(`shx rm -rf ${folderToServe}`);
+  execAndLog(`rm -rf ${folderToServe}`);
   // npm run build:public
-  execAndLog(`shx cp -r ${repoFolder}/public/ ${folderToServe}`, { cwd: repoFolder });
+  execAndLog(`cp -r ${repoFolder}/public/ ${folderToServe}`, { cwd: repoFolder });
+  execAndLog(`cp ${repoFolder}/vercel.json ${folderToServe}/vercel.json`, { cwd: repoFolder });
   // try copy some static assets, don't cause error if some of them been removed by the user
   try {
     // npm run build:public
-    execAndLog(`shx cp ${repoFolder}/tiddlers/favicon.ico ${folderToServe}/favicon.ico`, { cwd: repoFolder });
-    execAndLog(`shx cp ${repoFolder}/tiddlers/TiddlyWikiIconWhite.png ${folderToServe}/TiddlyWikiIconWhite.png`, {
+    execAndLog(`cp ${repoFolder}/tiddlers/favicon.ico ${folderToServe}/favicon.ico`, { cwd: repoFolder });
+    execAndLog(`cp ${repoFolder}/tiddlers/TiddlyWikiIconWhite.png ${folderToServe}/TiddlyWikiIconWhite.png`, {
       cwd: repoFolder,
     });
-    execAndLog(`shx cp ${repoFolder}/tiddlers/TiddlyWikiIconBlack.png ${folderToServe}/TiddlyWikiIconBlack.png`, {
+    execAndLog(`cp ${repoFolder}/tiddlers/TiddlyWikiIconBlack.png ${folderToServe}/TiddlyWikiIconBlack.png`, {
       cwd: repoFolder,
     });
   } catch (error) {
     console.log(error);
   }
   // npm run build:nodejs2html
-  execAndLog(`tiddlywiki ${repoFolder} --build externalimages`, { cwd: repoFolder });
+  // exclude edit related plugins, make it readonly, and reduce size
+  execAndLog(`tiddlywiki ${repoFolder} --build readonlyexternalimages`, { cwd: repoFolder });
   execAndLog(`tiddlywiki ${repoFolder} --build externaljs`, { cwd: repoFolder });
   // npm run build:sitemap
-  execAndLog(`tiddlywiki . --rendertiddler sitemap sitemap.xml text/plain && shx mv ${repoFolder}/output/sitemap.xml ${folderToServe}/sitemap.xml`, {
+  execAndLog(`tiddlywiki . --rendertiddler sitemap sitemap.xml text/plain && mv ${repoFolder}/output/sitemap.xml ${folderToServe}/sitemap.xml`, {
     cwd: repoFolder,
   });
   // npm run build:minifyHTML
@@ -45,7 +47,7 @@ module.exports = function build() {
   const htmlContent = fs.readFileSync(htmlMinifyPath, 'utf-8');
   const htmlContentWithCorrectJsPath = htmlContent.replaceAll('%24%3A%2Fcore%2Ftemplates%2Ftiddlywiki5.js', 'tiddlywiki5.js');
   fs.writeFileSync(htmlOutputPath, htmlContentWithCorrectJsPath);
-  execAndLog(`shx mv ${repoFolder}/output/tiddlywiki5.js ${folderToServe}/tiddlywiki5.js`, { cwd: repoFolder });
+  execAndLog(`mv ${repoFolder}/output/tiddlywiki5.js ${folderToServe}/tiddlywiki5.js`, { cwd: repoFolder });
   // npm run build:precache
   execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
 };
