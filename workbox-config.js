b2f64fb716b9a296c72392cdc3b9c0d471812d80
dongrentianyu <3465645884@qq.com>
2023-07-03 21:19:04 +0800

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -2,7 +2,7 @@ module.exports = {
   "globDirectory": "public-dist/",
   "maximumFileSizeToCacheInBytes": 1024 * 1024 * 256,
   "globPatterns": [
-    "**/*.{ico,html,js,json,png}"
+    "**/*.{ico,html,js,json,png,svg,jpg,jpeg,webp,gif,pdf,css,doc,docx,xsl,xslx,ppt,pptx,xml,opml,yaml,bmp,tiff,rtf,txt,heic,heif,woff,woff2,ogg,ogm,ogv,webm,mp4,mp3,m4a,md,tid,enex,bib,epub,octet-stream,multids,tiddler,recipe,htm,hta,m3u8,avi,rm,rmvb,wma,wav,mid,flv,ttf,otf,font,eot,fon,ttc}"
   ],
   "swDest": "public-dist/service-worker.js",
   "swSrc": "public/service-worker.js"
